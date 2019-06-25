2e2dbea1fccef81bd2cc7b787f8c63e8ea7e0cb1
Qbisst <ismaelaperez@outlook.com>
2019-06-25 11:43:36 -0400

    edited app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,71 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Details from "./pages/Details";
-import Post from "./pages/Post";
-import Saved from "./pages/Saved";
-import Search from "./pages/Search";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/post" component={Post} />
-          <Route exact path="/post/:id" component={Details} />
-          <Route exact path="/search" component={Search} />
-          {/* <Route exact path="/saved" component={Saved} /> */}
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Home from "./pages/Home";
+// import Details from "./pages/Details";
+// import Post from "./pages/Post";
+// import Saved from "./pages/Saved";
+// import Search from "./pages/Search";
+// import NoMatch from "./pages/NoMatch";
+// import Nav from "./components/Nav";
+
+
+// ---OLD CODE---//
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <Nav />
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route exact path="/post" component={Post} />
+//           <Route exact path="/post/:id" component={Details} />
+//           <Route exact path="/search" component={Search} />
+//           {/* <Route exact path="/saved" component={Saved} /> */}
+//           <Route component={NoMatch} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
 
