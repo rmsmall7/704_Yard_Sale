2fd19cf13583e874d1e33d6d5ad24ad18196bf2b
unknown <rmsmall7@gmail.com>
2019-06-25 15:49:47 -0400

    stash

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 import Post from "./pages/Post";
@@ -7,22 +7,26 @@ import Search from "./pages/Search";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/post" component={Post} />
-          <Route exact path="/post/:id" component={Details} />
-          <Route exact path="/search" component={Search} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/post" component={Post} />
+            <Route exact path="/post/:id" component={Details} />
+            <Route exact path="/search" component={Search} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  };
+};
 
 export default App;
 
