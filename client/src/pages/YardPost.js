cef8038fe2dc94a3b53d6d3acfdda02e11ed6ab0
unknown <rmsmall7@gmail.com>
2019-06-26 18:01:46 -0400

    updated homenew

---
+++ client/src/pages/YardPost.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class YardPost extends Component {
+  render() {
+    return (
+      <div>
+        <p>This yard post page is working</p>
+      </div>
+    )
+  }
+}
---
+++ client/src/pages/YardSave.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class YardSave extends Component {
+  render() {
+    return (
+      <div>
+        <p>This yard save page is working</p>
+      </div>
+    )
+  }
+}
