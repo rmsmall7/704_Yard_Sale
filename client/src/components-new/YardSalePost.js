6d1e6988138f4c21800cf13af80a802e0670dc6e
Natasha Henry <natashaphenry@yahoo.com>
2019-06-26 13:37:55 -0400

    corrected code

---
+++ client/src/components-new/YardSalePost.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class YardSalePost extends Component {
+    render() {
+        return (
+            <div className="col-xl-4 col-md-6">
+                <div className="product">
+                    <div className="product_content">
+                        <div className="product_info d-flex flex-row align-items-start justify-content-start">
+                            <div>
+                                <div>
+                                    <div className="product_name"><a href="#">Moving Sale!</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="product_buttons">
+                            <div className="text-right d-flex flex-row align-items-start justify-content-start">
+                                <div className="product_button product_fav text-center d-flex flex-column align-items-center justify-content-center">
+                                    <div>
+                                        <div><img src="images/heart_2.svg" className="svg" alt />
+                                            <div>+</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
