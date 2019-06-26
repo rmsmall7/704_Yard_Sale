be5b5709e6bb348159a5ec86be8ab4e94d12ea0d
Qbisst <ismaelaperez@outlook.com>
2019-06-25 21:03:45 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <Menu />
+        <Menu />
       <SuperContainer>
         <Header/>
       </SuperContainer>
