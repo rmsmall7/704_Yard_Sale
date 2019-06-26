6983c02ea2495f505143cdb5e4f1151d875f4616
Qbisst <ismaelaperez@outlook.com>
2019-06-26 14:19:13 -0400

    created home banner component

---
+++ client/src/components-new/HomeBanner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function HomeBanner(props) {
+    return (
+        <div classname="home">
+            <section id="banner">
+                <div classname="inner">
+                    <h1>704 Yard Sale</h1>
+                    <p id="innerText">Welcome to 704 Yard Sale, the fast and convenient way to find local yard sales
+        in your area!</p>
+                </div>
+                <video autoPlay loop muted playsInline src="images/drone.mp4">
+                </video></section>
+        </div>
+
+    );
+}
+
+export default HomeBanner;
