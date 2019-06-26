f72fbea195e92fef30f7ae4a1f1d361b9d901352
Qbisst <ismaelaperez@outlook.com>
2019-06-26 14:29:39 -0400

    finished landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from "react";
-  // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Menu from './components-new/Menu';
 import SuperContainer from './components-new/SuperContainer';
 import Header from './components-new/Header';
+import SuperContainerInner from './components-new/SuperContainerInner';
+import HomeBanner from './components-new/HomeBanner';
+import YardSaleHeader from './components-new/YardSaleHeader';
+import Row from './components-new/Row';
+import YardSalePost from './components-new/YardSalePost';
+import Footer from './components-new/Footer';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Menu />
-      <SuperContainer>
-        <Header/>
-        
-      </SuperContainer>
-      
+        <SuperContainer>
+          <Header />
+          <SuperContainerInner>
+            <HomeBanner></HomeBanner>
+            <YardSaleHeader></YardSaleHeader>
+            <Row></Row>
+            <YardSalePost></YardSalePost>
+            <Footer></Footer>
+          </SuperContainerInner>
+
+        </SuperContainer>
+
       </React.Fragment>
     );
 
