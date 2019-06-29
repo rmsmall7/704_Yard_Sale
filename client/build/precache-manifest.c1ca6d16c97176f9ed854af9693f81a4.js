1eb1321a3be30057c99511b00345df442ab1f2ec
unknown <rmsmall7@gmail.com>
2019-06-29 13:09:51 -0400

    updated package.json

---
+++ client/build/precache-manifest.c1ca6d16c97176f9ed854af9693f81a4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eabbddf187e9c2cbf6d6fc7e01bfc7ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0582c9bb66621c0a5c2e",
+    "url": "/static/js/2.c2368932.chunk.js"
+  },
+  {
+    "revision": "874f9d830dddfc1e242f",
+    "url": "/static/js/main.53e3c6c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Details from "./pages/Details";
-import Post from "./pages/Post";
-import Search from "./pages/Search";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-
-class App extends Component {
-=======
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Menu from './components/Menu';
@@ -28,20 +14,10 @@ class App extends Component {
     isLoggedIn: true
   }
   
->>>>>>> 8cd4e4e5100df2cf68d232d5fbe18f5b1fcd9d4f
   render() {
     return (
       <Router>
         <div>
-<<<<<<< HEAD
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/post" component={Post} />
-            <Route exact path="/post/:id" component={Details} />
-            <Route exact path="/search" component={Search} />
-            <Route component={NoMatch} />
-=======
           <Menu />
           <Header />
           <Switch>
@@ -49,19 +25,13 @@ class App extends Component {
             <Route exact path="/YardPost" component={YardPost} />
             <Route exact path="/YardSave" component={YardSave} />
             <Route exact path="/YardSaleList" component={YardSaleList} />
->>>>>>> 8cd4e4e5100df2cf68d232d5fbe18f5b1fcd9d4f
           </Switch>
         </div>
       </Router>
     );
-<<<<<<< HEAD
-  };
-};
-=======
 
   }
 }
->>>>>>> 8cd4e4e5100df2cf68d232d5fbe18f5b1fcd9d4f
 
 export default App;
 
