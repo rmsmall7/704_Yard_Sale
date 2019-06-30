eb2b7405faf16478d72392d9dc5540def6361d7f
Natasha Henry <natashaphenry@yahoo.com>
2019-06-30 12:28:23 -0400

    installed morgan

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
+const morgan = require('morgan')
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -13,6 +14,7 @@ const db = require('./config/keys').MongoURI;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+app.use(morgan);
 
 // Connect to Mongo
 mongoose.connect(db, { useNewUrlParser: true })
