9ad83edc04d74386e44543298eaa5c143121f3e1
unknown <rmsmall7@gmail.com>
2019-07-01 14:48:50 -0400

    update

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MongoURI: process.env.MONGODB_URI || process.env.MONGO
-}
+  MongoURI: 'mongodb://admin:admin1@cluster0-shard-00-00-3c16n.mongodb.net:27017,cluster0-shard-00-01-3c16n.mongodb.net:27017,cluster0-shard-00-02-3c16n.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+ }
